4b47a037e191fe29187642badab72bf8731a8aac
Abdullah Selek <abdullahselek@gmail.com>
2017-12-26 14:27:46 +0300

    add description for load file function

--- mergerpy/merger.py
+++ mergerpy/merger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 def load_file(filename):
+    """Loads given text file from directory into string lines"""
     with open(filename, 'r') as payload_file:
         lines = []
         for line in payload_file:
