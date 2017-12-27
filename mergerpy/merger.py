eaf15489cb787e5a1c7c5c3afe134ca8fb9bd5a6
Abdullah Selek <abdullahselek@gmail.com>
2017-12-27 11:09:27 +0300

    implement merge function which creates new file

--- mergerpy/merger.py
+++ mergerpy/merger.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from itertools import product
+
 def load_file(filename):
     """Loads given text file from directory into string lines"""
     with open(filename, 'r') as payload_file:
@@ -15,3 +17,14 @@ def parse_input(lines):
         parsed_data = line.split()
         parsed_input.append(parsed_data)
     return parsed_input
+
+def merge_input(input_array):
+    """Merges concatanates given input array into lines of strings"""
+    product(*input_array)
+    with open('merged.txt', 'w') as merged_file:
+        for input_list in product(*input_array):
+            merged_str = ''.join(str(e) for e in input_list)
+            merged_file.write(merged_str + '\n')
+    merged_file.close()
+    print('Merged file created successfully!')
+    return True
--- tests/test_merger.py
+++ tests/test_merger.py
@@ -13,3 +13,9 @@ class MergerTest(unittest.TestCase):
         lines = merger.load_file('tests/resources/sample.txt')
         parsed_input = merger.parse_input(lines)
         self.assertEqual(len(parsed_input), 3)
+
+    def test_merge_input(self):
+        lines = merger.load_file('tests/resources/sample.txt')
+        parsed_input = merger.parse_input(lines)
+        merged_file_created = merger.merge_input(parsed_input)
+        self.assertTrue(merged_file_created)        
