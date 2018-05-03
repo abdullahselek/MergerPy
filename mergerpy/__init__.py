495542a574dc0bdde3aa32bd65c6a91e08b0d21c
Abdullah Selek <abdullahselek@gmail.com>
2018-05-03 20:54:13 +0100

    Update download url.

--- mergerpy/__init__.py
+++ mergerpy/__init__.py
@@ -10,7 +10,7 @@ __copyright__    = 'Copyright (c) 2017 Abdullah Selek'
 __license__      = 'MIT License'
 __version__      = '0.2'
 __url__          = 'https://github.com/abdullahselek/MergerPy'
-__download_url__ = 'https://pypi.python.org/pypi/mergerpy/'
+__download_url__ = 'https://pypi.org/pypi/mergerpy/'
 __description__  = 'A library that provides text concatenation from given files'
 
 from mergerpy import merger
