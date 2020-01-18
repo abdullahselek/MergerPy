4c25c7a9bd3382ed34d49ebac21ad438c6603650
Abdullah Selek <abdullahselek@gmail.com>
2020-01-18 20:13:13 +0000

    Improve coding style and add type hints.

--- mergerpy/merger.py
+++ mergerpy/merger.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 import sys
-from itertools import product
 
-__version__ =  "0.1"
+from itertools import product
+from typing import List
+
+__version__ =  "0.2"
 
 def full_version():
     """Returns version of current mergerpy."""
+
     from struct import calcsize
     return "mergerpy {} on Python {} {}-bit, {}-bit unicodes".format(
         __version__,
@@ -15,33 +18,39 @@ def full_version():
         sys.maxunicode.bit_length(),
     )
 
-def load_file(filename):
+
+def load_file(filename: str):
     """Loads given text file from directory into string lines.
     Args:
       filename (str): Input file name.
     """
+
     with open(filename, 'r') as payload_file:
         lines = []
         for line in payload_file:
             lines.append(line)
         return lines
 
-def parse_input(lines):
+
+def parse_input(lines: List[str]):
     """Parse given input array that contains string lines.
     Args:
       lines (array): String lines of array.
     """
+
     parsed_input = []
     for line in lines:
         parsed_data = line.split()
         parsed_input.append(parsed_data)
     return parsed_input
 
-def merge_input(input_array):
+
+def merge_input(input_array: List):
     """Merges concatanates given input array into lines of strings.
     Args:
       input_array (array): Parsed string of array.
     """
+
     product(*input_array)
     with open('merged.txt', 'w') as merged_file:
         for input_list in product(*input_array):
@@ -51,11 +60,13 @@ def merge_input(input_array):
     print('Merged file created successfully!')
     return True
 
-def process(filename):
+
+def process(filename: str):
     """Merges texts given file from directory into a new string.
     Args:
       filename (str): Input file name.
     """
+
     lines = load_file(filename)
     parsed_input = parse_input(lines)
     new_str = ''
@@ -64,11 +75,13 @@ def process(filename):
         new_str += merged_str + ','
     return new_str[:-1]
 
-def main(filename):
+
+def main(filename: str):
     """Main function which combines all three worker functions for command line interface.
     Args:
       filename (str): Input file name.
     """
+
     lines = load_file(filename)
     parsed_input = parse_input(lines)
     return merge_input(parsed_input)
